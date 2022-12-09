ea343ff3c76bbaa297caab6b276174dab894e449
Roman Uragan <roman.gallyamov@gmail.com>
2022-12-09 17:27:43 +0400

    "main fixed"

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
-    var likes: Int = 162
+    var likes: Int = 1011
     var result: String = "Понравилось $likes "
-    if (likes % 10 == 1) {
+    if (likes % 10 == 1 && likes % 100 != 11) {
         result += "человеку"
     } else{
         result += "людям"
